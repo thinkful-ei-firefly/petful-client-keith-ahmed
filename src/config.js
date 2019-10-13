e433161f353f7077f76a14b136b74dacfdfff0f5
Ahmed <ahmed.max7586@gmail.com>
2019-10-13 04:15:10 -0400

    ready for deploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8080/api'
+    // API_ENDPOINT: 'http://localhost:8080/api'
+    API_ENDPOINT: 'https://petful-keith-ahmed.herokuapp.com/api'
 }
